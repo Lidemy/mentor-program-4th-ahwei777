3eb3b1e134c2315e47892e613d0ffd26ed8ba8ed
David <puho999@hotmail.com.tw>
2020-06-20 23:44:08 +0800

    hw1~6finished

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,21 @@
 function capitalize(str) {
-  
-}
+    let result = ""
+    let findLowerCase = false
+    for(let i = 0 ; i<str.length ; i++){
+      //若先找到大寫字母則直接 return 原字串
+      if(str[i]>="A" && str[i]<="Z"){
+        return str
+      }
+      //若找到小寫則轉為大寫，並設定跳出判斷
+      if(str[i]>="a" && str[i]<="z" && !find){
+            result += str[i].toUpperCase()
+            findLowerCase = true
+      }
+      else{
+          result += str[i]
+      }
+    }
+    return result
+  }
 
 console.log(capitalize('hello'));
